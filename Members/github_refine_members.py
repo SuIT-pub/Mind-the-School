aabdbb9aae28084a0c62fdca0ec267dcb82f4dbf
SuitIThub <suit.it.pub@gmail.com>
2024-05-27 12:06:06 +0200

    Refactor github_refine_members.py to improve file content update logic

--- Members/github_refine_members.py
+++ Members/github_refine_members.py
@@ -6,6 +6,7 @@ import pytz
 
 import requests
 import base64
+import json
 
 # Setze diese Variablen mit deinen Informationen
 GITHUB_TOKEN = os.getenv('GITHUB_TOKEN')  # Stelle sicher, dass du dein Token sicher speicherst
@@ -13,11 +14,11 @@ REPO_OWNER = 'SuIT-pub'
 REPO_NAME = 'Mind-the-School'
 FILE_PATH = 'game/members copy.csv'
 COMMIT_MESSAGE = 'updated members.csv'
+HEADERS = {'Authorization': f'token {GITHUB_TOKEN}', 'Accept': 'application/vnd.github.v3+json'}
 
 def get_file_sha(file_path, repo_owner, repo_name):
     url = f'https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{file_path}'
-    print(GITHUB_TOKEN)
-    response = requests.get(url, headers={'Authorization': f'token {GITHUB_TOKEN}'})
+    response = requests.get(url, headers=HEADERS)
     response.raise_for_status()
     return response.json()['sha'], response.json()['content']
 
@@ -27,13 +28,13 @@ def get_file_content(file_path):
 
 def update_file_content(file_path, new_content, sha, repo_owner, repo_name, commit_message):
     url = f'https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{file_path}'
-    encoded_content = base64.b64encode(new_content.encode('utf-8')).decode('utf-8')
+    encoded_content = base64.b64encode(new_content.encode()).decode()
     data = {
         'message': commit_message,
         'content': encoded_content,
         'sha': sha
     }
-    response = requests.put(url, json=data, headers={'Authorization': f'token {GITHUB_TOKEN}'})
+    response = requests.put(url, data=json.dumps(data), headers=HEADERS)
     response.raise_for_status()
     return response.json()
 
