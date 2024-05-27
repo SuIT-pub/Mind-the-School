5c1017dbd58520b82446737f079f8d5563b781e8
SuitIThub <suit.it.pub@gmail.com>
2024-05-27 11:50:33 +0200

    Refactor github_refine_members.py to add new line to current content

--- Members/github_refine_members.py
+++ Members/github_refine_members.py
@@ -45,8 +45,9 @@ def main():
     current_content = get_file_content(FILE_PATH)
     print(current_content)
     new_content = current_content + '\nNeue Zeile hinzugefügt durch Skript.'
-
     time.sleep(5)
+
+    sha = get_file_sha(FILE_PATH, REPO_OWNER, REPO_NAME)
     # Update die Datei im Repository
     update_file_content(FILE_PATH, new_content, sha, REPO_OWNER, REPO_NAME, COMMIT_MESSAGE)
     print('Datei erfolgreich aktualisiert.')
