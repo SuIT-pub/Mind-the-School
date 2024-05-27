0709c5dc2adb243b1595692b0bed3be54af03bd4
SuitIThub <suit.it.pub@gmail.com>
2024-05-27 12:14:21 +0200

    Refactor github_refine_members.py to remove unnecessary 'sha' parameter in update_file_content function

--- Members/github_refine_members.py
+++ Members/github_refine_members.py
@@ -34,8 +34,7 @@ def update_file_content(file_path, new_content, sha, repo_owner, repo_name, comm
     encoded_content = base64.b64encode(new_content.encode()).decode()
     data = {
         'message': commit_message,
-        'content': encoded_content,
-        'sha': sha
+        'content': encoded_content
     }
     response = requests.put(url, json=data, headers=HEADERS)
     response.raise_for_status()
