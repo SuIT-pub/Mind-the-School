c6ee6d4bd1fd736f6a7f3626f6d32044e46284c5
SuitIThub <suit.it.pub@gmail.com>
2024-05-29 13:33:33 +0200

    Refactor github_refine_members.py to handle conflicts when updating file content

--- Members/github_refine_members.py
+++ Members/github_refine_members.py
@@ -14,14 +14,19 @@ REPO_OWNER = 'SuIT-pub'
 REPO_NAME = 'Mind-the-School'
 FILE_PATH = 'game/members_copy.csv'
 COMMIT_MESSAGE = 'updated members.csv'
-HEADERS = {
+GITHUB_HEADER = {
     'Authorization': f'token {GITHUB_TOKEN}', 
     'Accept': 'application/vnd.github.v3+json'
     }
 
+PATREON_TOKEN = os.getenv('PATREON_TOKEN')
+PATREON_HEADER = {
+    'Authorization': f'Bearer {PATREON_TOKEN}',
+}
+
 def get_file_sha(file_path, repo_owner, repo_name):
     url = f'https://api.github.com/repos/{repo_owner}/{repo_name}/contents/{file_path}'
-    response = requests.get(url, headers=HEADERS)
+    response = requests.get(url, headers=GITHUB_HEADER)
     response.raise_for_status()
     return response.json()['sha']
 
@@ -33,17 +38,27 @@ def update_file_content(file_path, new_content, sha, repo_owner, repo_name, comm
         'content': encoded_content,
         'sha': sha
     }
-    response = requests.put(url, json=data, headers=HEADERS)
+    response = requests.put(url, json=data, headers=GITHUB_HEADER)
     # response.raise_for_status()
+    return response.json()
+
+def get_patreon_data():
+    url = 'https://www.patreon.com/api/oauth2/v2/campaigns/10492412/members'
+    params = {
+        'include': 'currently_entitled_tiers',
+        'fields[member]': 'full_name,patron_status'
+    }
+    response = requests.get(url, headers=PATREON_HEADER, params=params)
+    response.raise_for_status()
+    print(response.json())
     return response.json()
 
 def main():
     # Hole den aktuellen SHA-Wert der Datei
     sha = get_file_sha(FILE_PATH, REPO_OWNER, REPO_NAME)
     
-    print(sha)
-    time.sleep(20)
-    
+    get_patreon_data()
+
     # Hole den neuesten SHA-Wert der Datei
     latest_sha = get_file_sha(FILE_PATH, REPO_OWNER, REPO_NAME)
     
